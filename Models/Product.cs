369b1f9984ede830b57a7221cf242c0146f716d4
Alex Lopes <alexlr10@hotmail.com>
2020-08-07 17:39:29 -0300

    Adição de models a api

---
+++ Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiAspNetEF.Models {
+    public class Product {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatorio")]
+        [MaxLength(60, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        public string Title { get; set; }
+
+
+        [MaxLength(60, ErrorMessage = "Este campo deve conter no máximo 1024 caracteres")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatorio")]
+        [Range(1, int.MaxValue, ErrorMessage = "O preço deve ser maior que 0")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatorio")]
+        [Range(1, int.MaxValue, ErrorMessage = "Categoria Inválida")]
+        public int CategoryId { get; set; }//Propriedade que refencia a categoria no banco
+        public Category Category { get; set; }//Propriedade de navegaçao, onde poderia acessar informaçoes de categoria
+    }
+}
