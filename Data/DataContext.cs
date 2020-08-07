f4ccd137b80f3ecdbf9ae690b2fb5b1c5288a0cd
Alex Lopes <alexlr10@hotmail.com>
2020-08-07 17:47:49 -0300

    DataContext e EF adicionados

---
+++ Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using apiAspNetEF.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiAspNetEF.Data {
+    public class DataContext: DbContext { //Representaçao do banco de dados em memoria
+
+        public DataContext(DbContextOptions<DataContext> options): base(options {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
